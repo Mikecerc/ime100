738b28200042a82966ce1b6ec0d3f9fa3a708942
mikecerc <mikecerc1@outlook.com>
2023-11-02 19:17:03 -0400

    broken

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,29 @@
 #include "main.h"
 
-#define ARM_MOTOR_PORT 12
-#define LEFT_MOTOR_ONE_PORT 1
+#define ARM_MOTOR_PORT 15
+#define LEFT_MOTOR_ONE_PORT 20
 #define LEFT_MOTOR_TWO_PORT 19
-#define RIGHT_MOTOR_ONE_PORT 11
-#define RIGHT_MOTOR_TWO_PORT 13
-#define CLAW_MOTOR_PORT 6
+#define RIGHT_MOTOR_ONE_PORT 16
+#define RIGHT_MOTOR_TWO_PORT 14
+#define CLAW_MOTOR_PORT 13
 
-
+#define INERTIAL_SENSOR_PORT 11
 /**
  * A callback function for LLEMU's center button.
  *
  * When this callback is fired, it will toggle line 2 of the LCD text between
  * "I was pressed!" and nothing.
  */
-void on_center_button() {
+void on_center_button()
+{
 	static bool pressed = false;
 	pressed = !pressed;
-	if (pressed) {
+	if (pressed)
+	{
 		pros::lcd::set_text(2, "I was pressed!");
-	} else {
+	}
+	else
+	{
 		pros::lcd::clear_line(2);
 	}
 }
@@ -30,7 +34,8 @@ void on_center_button() {
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
  */
-void initialize() {
+void initialize()
+{
 	pros::lcd::initialize();
 	pros::lcd::set_text(1, "Hello PROS User!");
 
@@ -80,67 +85,195 @@ void autonomous() {}
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
-void opcontrol() {
+void opcontrol()
+{
+	int newHeading = 0;
+	int JoisticHeading = 0;
+	int chassisHeading;
+	int headingError;
+
+	double forward, straff, turning;
+	int temp;
+	float theta;
+	bool FieldCenteric = true;
 
 	double maxSlowVelocity = 30;
 	double maxVelocity = 100;
-	//controller object
+	// controller object
 	Controller controller;
-
-	//arm motor control objects
-	Motor armMotor(-ARM_MOTOR_PORT);
 	ControllerButton armUpButton(ControllerDigital::A);
 	ControllerButton armDownButton(ControllerDigital::B);
+	ControllerButton slowButton(ControllerDigital::L2);
+	ControllerButton resetHeading(ControllerDigital::down);
+	ControllerButton enableFieldCentric(ControllerDigital::left);
+	// arm motor control objects
+	Motor armMotor(-ARM_MOTOR_PORT);
 
-	//drivetrain
+	// inertial meausrement unit
+	pros::Imu inertialSensor(INERTIAL_SENSOR_PORT);
+
+	// drivetrain
 	Motor frontLeftMotor(LEFT_MOTOR_ONE_PORT, false, AbstractMotor::gearset::green, AbstractMotor::encoderUnits::degrees);
 	Motor backLeftMotor(LEFT_MOTOR_TWO_PORT, false, AbstractMotor::gearset::green, AbstractMotor::encoderUnits::degrees);
 	Motor frontRightMotor(RIGHT_MOTOR_ONE_PORT, true, AbstractMotor::gearset::green, AbstractMotor::encoderUnits::degrees);
 	Motor backRightMotor(RIGHT_MOTOR_TWO_PORT, true, AbstractMotor::gearset::green, AbstractMotor::encoderUnits::degrees);
-	ControllerButton slowButton(ControllerDigital::L2);
-	
+
+	// reset IMU
+	inertialSensor.reset();
+
 	// Chassis Controller - lets us drive the robot around with open- or closed-loop control
 	std::shared_ptr<ChassisController> chassis = ChassisControllerBuilder()
-	        .withMotors(
-				 frontLeftMotor, frontRightMotor, backRightMotor, backLeftMotor   // Bottom left
-			)
-	        // Green gearset, 4 in wheel diam, 11.5 in wheel track
-	        .withDimensions(AbstractMotor::gearset::green, {{4_in, 12_in}, imev5GreenTPR})
-			.withOdometry()
-	        .buildOdometry();
-	
-	auto xModel = std::dynamic_pointer_cast<XDriveModel>(chassis->getModel());
+													 .withMotors(
+														 frontLeftMotor, frontRightMotor, backRightMotor, backLeftMotor // Bottom left
+														 )
+													 // Green gearset, 4 in wheel diam, 11.5 in wheel track
+													 .withDimensions(AbstractMotor::gearset::green, {{4_in, 12_in}, imev5GreenTPR})
+													 .withOdometry()
+													 .buildOdometry();
+
+	std::shared_ptr<okapi::XDriveModel> driveTrain = std::dynamic_pointer_cast<XDriveModel>(chassis->getModel());
 	/*std::shared_ptr<ChassisController> drive =
-	    ChassisControllerBuilder()
-	        .withMotors(
+		ChassisControllerBuilder()
+			.withMotors(
 				 frontLeftMotor, frontRightMotor, backRightMotor, backLeftMotor   // Bottom left
 			)
-	        // Green gearset, 4 in wheel diam, 11.5 in wheel track
-	        .withDimensions(AbstractMotor::gearset::green, {{4_in, 11.5_in}, imev5GreenTPR})
+			// Green gearset, 4 in wheel diam, 11.5 in wheel track
+			.withDimensions(AbstractMotor::gearset::green, {{4_in, 11.5_in}, imev5GreenTPR})
 			.withOdometry()
-	        .buildOdometry();
+			.buildOdometry();
 */
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-    // Tank drive with left and right sticks.
-	double leftX = slowButton.isPressed() ? controller.getAnalog(ControllerAnalog::leftX) / 4 : controller.getAnalog(ControllerAnalog::leftX);
-	double leftY = slowButton.isPressed() ? controller.getAnalog(ControllerAnalog::leftY) / 4 : controller.getAnalog(ControllerAnalog::leftY);
-	double rightX = slowButton.isPressed() ? controller.getAnalog(ControllerAnalog::rightX) / 4 : controller.getAnalog(ControllerAnalog::rightX);
-
-
-    xModel->xArcade(leftX, leftY, rightX);
-		pros::delay(10);
-	
-	if (armUpButton.isPressed()) {
-                armMotor.moveVoltage(12000);
-            } else if (armDownButton.isPressed()) {
-                armMotor.moveVoltage(-12000);
-            } else {
-                armMotor.moveVoltage(0);
-            }
-
+	while (inertialSensor.is_calibrating())
+	{
+		pros::delay(20);
 	}
+	while (true)
+	{;
+		chassisHeading = inertialSensor.get_heading() - headingError;
+		printf("Heading: %f\n", chassisHeading);
+		pros::lcd::print(3, "Heading: %f", chassisHeading);
+		forward = controller.getAnalog(ControllerAnalog::leftY) * 100;
+		/**if (abs(forward) < 0.3)
+		{
+			forward = 0;
+		}*/
+		straff = controller.getAnalog(ControllerAnalog::leftX) * 100;
+		/**if (abs(straff) < 0.3)
+		{
+			straff = 0;
+		}*/
+		turning = controller.getAnalog(ControllerAnalog::rightX) * 100;
+		/*if (abs(turning) < 0.3)
+		{
+			turning = 0;
+		}*/
 
+		theta = (-chassisHeading * 3.1415926535) / 180;
+		temp = forward * cos(theta) - straff * sin(theta);
+		straff = forward * sin(theta) + straff * cos(theta);
+		forward = temp;
+		std::cout << inertialSensor.get_pitch() << std::endl;
+		if (inertialSensor.get_pitch() < -10)
+		{
+			driveTrain->forward(-30);
+			pros::delay(200);
+		}
+		if (enableFieldCentric.changedToPressed())
+		{
+			if (FieldCenteric == true)
+			{
+				controller.setText(1, 8, "False");
+				FieldCenteric = false;
+			}
+			else
+			{
+				controller.setText(1, 8, "True");
+				FieldCenteric = true;
+			}
+		}
+		if (resetHeading.isPressed())
+		{
+			headingError = inertialSensor.get_heading();
+		}
+
+		int TargetStraff = FieldCenteric ? straff / 100 : controller.getAnalog(ControllerAnalog::leftX);
+		int TargetForward = FieldCenteric ? forward / 100 : controller.getAnalog(ControllerAnalog::leftY);
+		int TargetTurning = FieldCenteric ? turning / 100 : controller.getAnalog(ControllerAnalog::rightX);
+
+		TargetStraff = slowButton.isPressed() ? TargetStraff / 4 : TargetStraff;
+		TargetForward = slowButton.isPressed() ? TargetForward / 4 : TargetForward;
+		TargetTurning = slowButton.isPressed() ? TargetTurning / 4 : TargetTurning;
+
+		driveTrain->xArcade(TargetStraff, TargetForward, TargetTurning);
+
+		if (armUpButton.isPressed())
+		{
+			armMotor.moveVoltage(12000);
+		}
+		else if (armDownButton.isPressed())
+		{
+			armMotor.moveVoltage(-12000);
+		}
+		else
+		{
+			armMotor.moveVoltage(0);
+		}
+	}
+	/**
+	while (true)
+	{
+		// print values to display
+		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
+						 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
+						 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
+		pros::lcd::print(2, "%f", inertialSensor.get());
+
+		// mecanum drive with left and right sticks.
+
+		// raw values from controller
+		double leftXRaw = controller.getAnalog(ControllerAnalog::leftX);
+		double leftYRaw = controller.getAnalog(ControllerAnalog::leftY);
+		double rightXRaw = controller.getAnalog(ControllerAnalog::rightX);
+
+		// clip values close to 0
+		const int deadband = 15;
+		if (abs(leftXRaw) < deadband)
+			leftXRaw = 0;
+		if (abs(leftYRaw) < deadband)
+			leftYRaw = 0;
+		if (abs(rightXRaw) < deadband)
+			rightXRaw = 0;
+
+		double theta = degToRad(inertialSensor.get());
+
+		int temp = leftXRaw * sin(theta) + leftYRaw * cos(theta);
+		leftXRaw = leftXRaw * cos(theta) - leftYRaw * sin(theta);
+		leftYRaw = temp;
+		// scale values to max velocity
+		double leftX = slowButton.isPressed() ? leftXRaw / 4 : leftXRaw;
+		double leftY = slowButton.isPressed() ? leftYRaw / 4 : leftYRaw;
+		double rightX = slowButton.isPressed() ? rightXRaw / 4 : rightXRaw;
+
+		xModel->xArcade(leftX, leftY, rightX);
+
+		if (armUpButton.isPressed())
+		{
+			armMotor.moveVoltage(12000);
+		}
+		else if (armDownButton.isPressed())
+		{
+			armMotor.moveVoltage(-12000);
+		}
+		else
+		{
+			armMotor.moveVoltage(0);
+		}
+		// sleep at end of loop
+		pros::delay(10);
+	}
+	*/
+}
+
+double degToRad(double deg)
+{
+	return deg * 3.14159265358979323846 / 180;
 }
